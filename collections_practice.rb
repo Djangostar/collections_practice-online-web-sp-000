532e758cec0caedcf707c49768f68e267105509a
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-16 21:06:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,8 +11,7 @@ def sort_array_char_count(string)
 end
 
 def swap_elements(arr)
-  arr[1], arr[2] = arr[2], arr[1]
-  arr
+
 end
 
 def reverse_array(arr)
@@ -20,26 +19,17 @@ def reverse_array(arr)
 end
 
 def kesha_maker(arr)
-  new_arr = []
-  arr.each do |sign|
-    new_arr << sign[2] = "$"
-  end
+
 end
 
 def find_a(arr)
-  arr.select do |word|
-    word.start_with?('a')
-  end
+
 end
 
 def sum_array(arr)
-  arr.inject do |sum, num|
-    sum + num
-  end
+
 end
 
 def add_s(array)
-  array.collect do |word|
-    array[1] == word ? word : word + "s"
-  end
+
 end
